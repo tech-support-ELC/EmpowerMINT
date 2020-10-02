07e2609a03fc6a70e83bcf4e9e2c3171ea4bf118
Ariel Ng <ng.ariel.elizabeth@gmail.com>
2020-10-02 18:05:59 -0400

    Feat: Sign up using Google Authentication

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -6,9 +6,22 @@ const SignUp = ({ history }) => {
   const handleSignUp = useCallback(
     async (event) => {
       event.preventDefault();
-      const { email, password } = event.target.elements;
+      const { email, password, username } = event.target.elements;
+      // const userRef = app.database().ref('users')
+      // const user = {
+      //  email, 
+      //  password
+      // }
+
       try {
         await app.auth().createUserWithEmailAndPassword(email.value, password.value);
+        const UID = app.auth().currentUser.uid    
+        const apple = {"apple":"appleTEST"}
+        app.database().ref('users').push(
+          // {[UID] : {"TEST":"TEST"} }
+          apple
+        )
+        console.log(app.auth().currentUser.uid) //GETS USER ID FROM AUTHENTICAITON 
         history.push("/home");
       } catch (error) {
         alert(error);
@@ -25,10 +38,51 @@ const SignUp = ({ history }) => {
           email
           <input name="email" type="email" placeholder="Email" />
         </label>
+
         <label>
           password
           <input name="password" type="password" placeholder="Password" />
         </label>
+
+        <label>
+          username
+          <input name="username" type="username" placeholder="Username" />
+        </label>
+
+        <div>
+        <label>
+          pronouns
+        </label>
+
+          <select> 
+          <option>she/hers</option>
+          <option>him/his</option>
+          <option>they/them</option>
+          </select>
+        </div>
+
+        <div>
+        <label>
+          I am a:
+        </label>
+
+          <select> 
+          <option>Survivor</option>
+          <option>Supporter</option>
+          <option>Warrior - Stage 1</option>
+          <option>Warrior - Stage 2</option>
+          <option>Warrior - Stage 3</option>
+          <option>Warrior - Stage 4</option>
+          </select>
+        </div>
+
+        <div>
+        <label>
+          Bio/Interests
+          <input name="Bio/Interests" type="text" placeholder="Bio/Interests" />
+        </label>
+        </div>
+
         <button type="submit">Sign Up</button>
       </form>
     </div>
