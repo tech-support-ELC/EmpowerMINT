0886e84c927c11e3b8725a5effd893f336eaa801
Kristi Nanco <kristiknanco@gmail.com>
2020-10-02 12:05:55 -0400

    feat: add icons to header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import './Header.css'
+import PersonIcon from '@material-ui/icons/Person'
+import ChatIcon from '@material-ui/icons/Chat'
 
 function Header() {
   return (
-    <div>
-      <h2>I'm the header</h2>
+    <div className="header">
+      <PersonIcon />
+      <img src="https://cdn.pixabay.com/photo/2016/08/31/11/54/user-1633249_960_720.png" alt="tinder-logo" />
+      <ChatIcon />
     </div>
   )
 }
