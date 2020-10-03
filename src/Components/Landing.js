550785c4e843483c53afdaabab7fab3962d902d7
esk <emseely@gmail.com>
2020-10-03 16:14:23 -0700

    feat: signup refactored to bootstrap modal form, landing page init

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,10 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import Card from "react-bootstrap/Card";
+import { SignUp, LogIn } from "./index";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Nav from "react-bootstrap/Nav";
 
 function Landing() {
+  const [showSignup, setSignup] = useState(false);
+  const [showLogin, setLogin] = useState(false);
+
+  const handleClose = () => {
+    setLogin(false);
+    setSignup(false);
+  };
+  const handleLogin = () => setLogin(true);
+  const handleSignup = () => setSignup(true);
   return (
     <div>
-      <h1>Landing</h1>
-      <footer>LANDING</footer>;
+      <Card>
+        <Card.Header>
+          <Nav variant="tabs" defaultActiveKey="#first">
+            <Nav.Item>
+              <Nav.Link href="#first">Log in/Sign up</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#link">About EmpowerMINT</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Card.Header>
+        <Card.Body>
+          <Card.Title> Welcome to EncourageMINT,</Card.Title>
+          <Card.Text>
+            a community of people affected by breast cancer. Join now to be
+            matched with community members based on the kind of support YOU ask
+            for.
+          </Card.Text>
+          <Button variant="light" onClick={handleLogin}>
+            Log In
+          </Button>
+          <Button variant="light" onClick={handleSignup}>
+            Sign Up
+          </Button>
+        </Card.Body>
+      </Card>
+
+      <Modal show={showSignup} onHide={handleClose} animation={false}>
+        <SignUp onHide={handleClose} />
+      </Modal>
+
+      <Modal show={showLogin} onHide={handleClose} animation={false}>
+        <LogIn onHide={handleClose} />
+      </Modal>
     </div>
   );
 }
--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,6 +1,11 @@
 import React, { useCallback } from "react";
 import { withRouter } from "react-router";
 import app from "../firebase";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const SignUp = ({ history }) => {
   const handleSignUp = useCallback(
@@ -26,60 +31,124 @@ const SignUp = ({ history }) => {
 
   return (
     <div>
-      <h1>Sign Up</h1>
-      <form onSubmit={handleSignUp}>
-        <label>
-          email
-          <input name="email" type="email" placeholder="Email" />
-        </label>
+      <Modal.Header closeButton>
+        <Modal.Title>Sign Up</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={handleSignUp}>
+          <Row>
+            <Col>
+              <Form.Group controlId="newName">
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="string" placeholder="New display name" />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group controlId="newEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="name@example.com" />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group controlId="newPass">
+                <Form.Label>Password </Form.Label>
+                <Form.Control
+                  type="string"
+                  placeholder="secretPasswordLookAway!"
+                />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group controlId="newPronouns">
+                <Form.Label>Pronouns</Form.Label>
+                <Form.Control type="string " placeholder="They/them/theirs" />
+              </Form.Group>
+            </Col>
+          </Row>
 
-        <label>
-          password
-          <input name="password" type="password" placeholder="Password" />
-        </label>
-
-        <label>
-          username
-          <input name="username" type="username" placeholder="Username" />
-        </label>
-
-        <div>
-          <label>pronouns</label>
-
-          <select>
-            <option>she/hers</option>
-            <option>him/his</option>
-            <option>they/them</option>
-          </select>
-        </div>
-
-        <div>
-          <label>I am a:</label>
-
-          <select>
-            <option>Survivor</option>
-            <option>Supporter</option>
-            <option>Warrior - Stage 1</option>
-            <option>Warrior - Stage 2</option>
-            <option>Warrior - Stage 3</option>
-            <option>Warrior - Stage 4</option>
-          </select>
-        </div>
-
-        <div>
-          <label>
-            Bio/Interests
-            <input
-              name="Bio/Interests"
-              type="text"
-              placeholder="Bio/Interests"
-            />
-          </label>
-        </div>
-
-        <button type="submit">Sign Up</button>
-      </form>
+          <Form.Group controlId="newType">
+            <Form.Label>
+              Choose the identifier that best describes you
+            </Form.Label>
+            <Form.Control as="select" multiple>
+              <option>Survivor</option>
+              <option>Supporter</option>
+              <option>Warrior - Stage 1</option>
+              <option>Warrior - Stage 2</option>
+              <option>Warrior - Stage 3</option>
+              <option>Warrior - Stage 4</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId="newBio">
+            <Form.Label>Bio</Form.Label>
+            <Form.Control as="textarea" rows="3" />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="light" type="submit">
+          Sign Up
+        </Button>
+      </Modal.Footer>
     </div>
+
+    // <div>
+    //   <h1>Sign Up</h1>
+    //   <form onSubmit={handleSignUp}>
+    //     <label>
+    //       email
+    //       <input name="email" type="email" placeholder="Email" />
+    //     </label>
+
+    //     <label>
+    //       password
+    //       <input name="password" type="password" placeholder="Password" />
+    //     </label>
+
+    //     <label>
+    //       username
+    //       <input name="username" type="username" placeholder="Username" />
+    //     </label>
+
+    //     <div>
+    //       <label>pronouns</label>
+
+    //       <select>
+    //         <option>she/hers</option>
+    //         <option>him/his</option>
+    //         <option>they/them</option>
+    //       </select>
+    //     </div>
+
+    //     <div>
+    //       <label>I am a:</label>
+
+    //       <select>
+    //         <option>Survivor</option>
+    //         <option>Supporter</option>
+    //         <option>Warrior - Stage 1</option>
+    //         <option>Warrior - Stage 2</option>
+    //         <option>Warrior - Stage 3</option>
+    //         <option>Warrior - Stage 4</option>
+    //       </select>
+    //     </div>
+
+    //     <div>
+    //       <label>
+    //         Bio/Interests
+    //         <input
+    //           name="Bio/Interests"
+    //           type="text"
+    //           placeholder="Bio/Interests"
+    //         />
+    //       </label>
+    //     </div>
+
+    //     <button type="submit">Sign Up</button>
+    //   </form>
+    // </div>
   );
 };
 
