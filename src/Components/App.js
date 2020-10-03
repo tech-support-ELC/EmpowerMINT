a0986ea6130ac12a9a5f5d53f5268ebe14269861
esk <emseely@gmail.com>
2020-10-03 10:56:16 -0700

    merge

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,7 @@
 import React from "react";
 import { Header, Routes, Footer } from "./index";
-<<<<<<< HEAD
-import "./style/App.scss";
-=======
 import "./App.css";
 import { AuthProvider } from "./Auth";
->>>>>>> 719d6855b017b0b7042e8ac73f286b378c585e6b
 
 function App() {
   return (
