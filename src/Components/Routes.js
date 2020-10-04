a7297ce51fdc594c654f4a8bb00ccba3ebb2c253
Kristi Nanco <kristiknanco@gmail.com>
2020-10-03 20:05:32 -0400

    fix: add chats routes

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -1,31 +1,23 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import {
-  Home,
-  Landing,
-  ChatList,
-  SignUp,
-  Profile,
-  LogIn,
-  PrivateRoute,
-} from "./index";
+import React from 'react'
+import {Route, Switch} from 'react-router-dom'
+import {Home, Landing, ChatList, SignUp, Profile, LogIn, PrivateRoute, ChatScreen} from './index'
 
 const Routes = () => {
   return (
     <div id="main">
-      <div id="bg"></div>
+      <div id="bg" />
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route path="/signup" component={SignUp} />
         <Route path="/login" component={LogIn} />
         <PrivateRoute exact path="/home" component={Home} />
-
-        {/* <Route path="/chats" component={ChatList} />
-        <Route path="/signup" component={SignUp} />*/}
+        <Route path="/chats/:person" component={ChatScreen} />
+        <Route path="/chats" component={ChatList} />
+        <Route path="/signup" component={SignUp} />
         <Route path="/profile" component={Profile} />
       </Switch>
     </div>
-  );
-};
+  )
+}
 
-export default Routes;
+export default Routes
